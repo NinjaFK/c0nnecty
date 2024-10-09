89bd53455b3cca9e04b38760f927265c24cc3f60
Brian <nieh_brian@yahoo.com>
2024-10-09 13:14:26 -0700

    Created board and display

--- main.cpp
+++ main.cpp
@@ -10,9 +10,54 @@ public:
 class Board
 {
 public:
-    int board[9];
+    int board[6][7];
     int turn;
     std::vector<Move> history;
+    const std::string reset = "\033[0m";
+    const std::string r = "\033[31m";
+    const std::string y = "\033[33m";
+    const std::string b = "\033[34m";
+
+    Board()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    // Red is 1
+    // Yellow is 2
+
+    void displayBoard()
+    {
+        board[5][0] = 1;
+        board[5][1] = 2;
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                std::cout << b << "[";
+                if (board[i][j] == 1)
+                {
+                    std::cout << r << 'o';
+                }
+                else if (board[i][j] == 2)
+                {
+                    std::cout << y << 'o';
+                }
+                else
+                {
+                    std::cout << reset << ' ';
+                }
+                std::cout << b << "]";
+            }
+            std::cout << '\n';
+        }
+    }
 };
 
 // eval
@@ -37,6 +82,8 @@ int main()
     Board game;
     bool stillPlaying = false;
     int pos = 0;
+
+    game.displayBoard();
 
     while (true)
     {
