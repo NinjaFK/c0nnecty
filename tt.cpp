a26e76a83eabf2bf088a9f1fd183adb8de82760b
Brian <nieh_brian@yahoo.com>
2025-04-23 21:24:54 -0700

    Clear and score

--- tt.cpp
+++ tt.cpp
@@ -1,8 +1,11 @@
 #include "include.h"
+
 struct TTEntry
 {
     uint64_t hash = 0;
     Move bestMove = Move(0, 0);
+    uint8_t depth = 0;
+    uint32_t score = 0;
 };
 
 struct TransTable
@@ -12,9 +15,9 @@ private:
     uint32_t numEntries;
 
 public:
-    TransTable(uint32_t mega = 0)
+    TransTable(uint32_t mega = 16)
     {
-        numEntries = (mega * 1024) / (sizeof(TTEntry) * 1024);
+        numEntries = (mega * 1024) / (sizeof(TTEntry)) * 1024;
         if (mega != 0)
         {
             table.resize(numEntries);
@@ -29,5 +32,13 @@ public:
     void insert(TTEntry entry)
     {
         table[entry.hash % numEntries] = entry;
+    }
+
+    void clear()
+    {
+        for (int i = 0; i < numEntries; i++)
+        {
+            table[i] = TTEntry();
+        }
     }
 };
